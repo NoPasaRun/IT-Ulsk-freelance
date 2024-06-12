74267a1464c5f6395fbd68d6eeab476101c4b7fb
bogdan <bogdanbelenesku@gmail.com>
2024-06-12 12:51:42 +0300

    fixes

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -4,14 +4,6 @@ from app import create_pagination, generate_distribution, generate_histogram_y
 import pytest
 
 from models import Company
-
-CASES = [
-    (2, 4, (1, 2, 3, 4)),
-    (5, 9, (1, "...", 4, 5, 6, "...", 9)),
-    (4, 17, (1, 2, 3, 4, 5, "...", 17)),
-    (18, 20, (1, "...", 17, 18, 19, 20))
-]
-
 CASES_2 = [
     (
         [
@@ -40,16 +32,6 @@ CASES_3 = [
         [f"{num} млн" for num in [36.45, 37.69, 38.93, 40.16, 42.64]]
     )
 ]
-
-
-@pytest.mark.parametrize("current_page,total_pages,expected", CASES)
-def test_pagination(current_page: int, total_pages: int, expected: tuple):
-    texts = tuple(
-        data.get("text") for data in create_pagination(
-            "", current_page, total_pages, ""
-        )
-    )
-    assert texts == expected
 
 
 @pytest.mark.parametrize("queryset,attr_name,limit,expected", CASES_2)
